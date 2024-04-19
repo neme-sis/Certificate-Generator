50123760660eb8988827acb525e14b248a64a8c3
neme-sis <ghoshsayan894@gmail.com>
2024-04-19 17:08:22 +0530

    Add adjustOverflows function to handle text overflow on canvas

--- src/helpers/CanvasControllers.js
+++ src/helpers/CanvasControllers.js
@@ -15,9 +15,49 @@ function loadTextOnCanvas(ctx, text, style, canvas) {
   ctx.fillText(text, style.getWidth(canvas), style.getHeight(canvas));
 }
 
+function adjustOverflows(textsWithStyles, canvas) {
+  const updatedTextsWithStyles = []
+  textsWithStyles.map((textWithStyle) => {
+    const width = 1.25 * canvas.width;
+    const style = textWithStyle.style ? textWithStyle.style : defaultStyle;
+    const fontSize = parseInt(style.fontSize);
+
+    if (width - 100 > fontSize * textWithStyle.text.length)
+      return updatedTextsWithStyles.push(textWithStyle);
+
+    const words = textWithStyle.text.trim().split(" ");
+    const brokenSentences = [];
+    let sentence = "";
+    words.forEach((word, index) => {
+      if(index === 0) {
+        sentence = word;
+        return;
+      }
+      if (width - 100 > fontSize * sentence.length + fontSize * (word.length + 1)) {
+        sentence += ` ${word}`;
+      } else {
+        brokenSentences.push(sentence);
+        sentence = word;
+      }
+    });
+    brokenSentences.push(sentence);
+    brokenSentences.forEach((sentence, index) => {
+      updatedTextsWithStyles.push({
+        text: sentence,
+        style: {
+          ...style,
+          getHeight: (canvas) => style.getHeight(canvas) + (index * (fontSize + 10)),
+        },
+      });
+    });
+  });
+  return updatedTextsWithStyles;
+}
+
 export function loadUpdatedCanvas(canvas, imgUrl, textsWithStyles = []) {
   //to omit the flicker between changing the background and the image
   canvas.style.backgroundImage = `url(${canvas.toDataURL("image/png")})`;
+
   let ctx = canvas.getContext("2d");
   ctx.clearRect(0, 0, canvas.width, canvas.height);
   ctx = canvas.getContext("2d");
@@ -27,6 +67,9 @@ export function loadUpdatedCanvas(canvas, imgUrl, textsWithStyles = []) {
     canvas.width = certificateImage.width;
     canvas.height = certificateImage.height;
     ctx.drawImage(certificateImage, 0, 0);
+
+    textsWithStyles = adjustOverflows(textsWithStyles, canvas);
+
     textsWithStyles.forEach((textWithStyle) => {
       loadTextOnCanvas(
         ctx,
